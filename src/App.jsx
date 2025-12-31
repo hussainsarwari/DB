f67a31199f6ae13a7acadcf797ba0632dd9b100e
Milad Rahimi <milad@yrmp.af>
2025-12-31 16:26:32 +0430

    starting my work

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import ResetPassword from './Login/ResetPasswor'
 import Home from './home/mainPage'
 import Production_management from './production_management/main_page'
 import SellPage from './sell/main'
+import Staff from "./staff/Staff";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/Reset-password" element={<ResetPassword/>}/>
-      <Route path="/home" element={<Home />}/>
-      <Route path="/Production_management" element={<Production_management />}/>
-      <Route path="/sell" element={<SellPage />}/>
+      <Route path="/Reset-password" element={<ResetPassword />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/Production_management" element={<Production_management />} />
+      <Route path="/sell" element={<SellPage />} />
+      <Route path="/staff" element={<Staff />} />
 
     </Routes>
   );
---
+++ src/staff/Staff.jsx
@@ -0,0 +1,5 @@
+function Staff() {
+    return <div>Staff Page</div>;
+}
+
+export default Staff;
